fa969ee7568e328e7a34a59f6131debe71239474
xmongkk <95506088+xmongkk@users.noreply.github.com>
2024-10-31 02:33:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TL;
+
+namespace WTelegramClient_session_Sqllite
+{
+	//t.me/nader5
+    static class Program
+    {
+        static WTelegram.Client Client;
+        static User My;
+        static Dictionary<long, User> Users = new();
+        static Dictionary<long, ChatBase> Chats = new();
+
+		static async Task Main(string[] _)
+		{
+			var exit = new SemaphoreSlim(0);
+			AppDomain.CurrentDomain.ProcessExit += (s, e) => exit.Release(); 
+		    var store = new SqliteStore("SQLLiteDatabase.db", "session1");
+		
+			Client = new WTelegram.Client(what => Config(what), store);
+			
+			await using (Client)
+			{
+				Client.OnUpdates += Client_OnUpdates;
+				My = await Client.LoginUserIfNeeded();
+				Console.WriteLine($"We are logged-in as {My.username ?? My.first_name + " " + My.last_name} (id {My.id})");
+				var dialogs = await Client.Messages_GetAllDialogs();
+				dialogs.CollectUsersChats(Users, Chats);
+				await exit.WaitAsync();
+			}
+		}
+		private static async Task Client_OnUpdates(UpdatesBase updates)
+		{
+			updates.CollectUsersChats(Users, Chats);
+			foreach (var update in updates.UpdateList)
+			{
+				Console.WriteLine(update.GetType().Name);
+				if (update is UpdateNewMessage { message: Message { peer_id: PeerUser { user_id: var user_id } } msg }) // private message
+					if (!msg.flags.HasFlag(Message.Flags.out_)) 
+						if (Users.TryGetValue(user_id, out var user))
+						{
+							Console.WriteLine($"New message from {user}: {msg.message}");
+							if (msg.message.Equals("Ping", StringComparison.OrdinalIgnoreCase))
+								await Client.SendMessageAsync(user, "Pong");
+						}
+			}
+		}
+        public static string Config(string what)
+        {
+
+
+            switch (what)
+            {
+      
+
+                case "api_id": return "api_id"; 
+				case "api_hash": return "phone_number";
+				case "phone_number": return "+phone_number";
+                case "first_name": return "nader";
+				case "verification_code": Console.Write("Code: "); return Console.ReadLine();
+				case "last_name": return "last_name";
+           
+                // if sign-up is required
+                case "password": return "1426";     // if user has enabled 2FA
+                default: return null;                  // let WTelegramClient decide the default config
+            }
+
+        }
+
+    }
+}
---
+++ SqliteStore.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace WTelegramClient_session_Sqllite
+{
+  public class SqliteStore : Stream
+    {
+        private readonly SqliteConnection _sql;
+        private readonly string _sessionName;
+        private byte[] _data;
+        private int _dataLen;
+        private DateTime _lastWrite;
+        private Task _delayedWrite;
+
+        public SqliteStore(string databasePath, string sessionName = "DefaultSession")
+        {
+            _sessionName = sessionName;
+            _sql = new SqliteConnection($"Data Source={databasePath};");
+            _sql.Open();
+
+            using (var create = new SqliteCommand("CREATE TABLE IF NOT EXISTS Sessions (name TEXT NOT NULL PRIMARY KEY, data BLOB)", _sql))
+                create.ExecuteNonQuery();
+
+            using var cmd = new SqliteCommand("SELECT data FROM Sessions WHERE name = @name", _sql);
+            cmd.Parameters.AddWithValue("@name", _sessionName);
+            using var rdr = cmd.ExecuteReader();
+            if (rdr.Read())
+                _dataLen = (_data = rdr["data"] as byte[])?.Length ?? 0;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _delayedWrite?.Wait();
+            _sql.Dispose();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            Array.Copy(_data, 0, buffer, offset, count);
+            return count;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _data = buffer;
+            _dataLen = count;
+
+            if (_delayedWrite != null) return;
+
+            var left = 1000 - (int)(DateTime.UtcNow - _lastWrite).TotalMilliseconds;
+            if (left < 0)
+            {
+                using var cmd = new SqliteCommand("INSERT OR REPLACE INTO Sessions (name, data) VALUES (@name, @data)", _sql);
+                cmd.Parameters.AddWithValue("@name", _sessionName);
+                cmd.Parameters.AddWithValue("@data", buffer.Length == count ? buffer : buffer[offset..(offset + count)]);
+                cmd.ExecuteNonQuery();
+                _lastWrite = DateTime.UtcNow;
+            }
+            else
+            {
+                _delayedWrite = Task.Delay(left).ContinueWith(t =>
+                {
+                    lock (this)
+                    {
+                        _delayedWrite = null;
+                        Write(_data, 0, _dataLen);
+                    }
+                });
+            }
+        }
+
+        public override long Length => _dataLen;
+        public override long Position { get => 0; set { } }
+        public override bool CanSeek => false;
+        public override bool CanRead => true;
+        public override bool CanWrite => true;
+        public override long Seek(long offset, SeekOrigin origin) => 0;
+        public override void SetLength(long value) { }
+        public override void Flush() { }
+    }
+}
